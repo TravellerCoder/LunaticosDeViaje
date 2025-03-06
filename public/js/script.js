3d10751ed0bcaaad675887cc28d5e004e7720783
Traveller Coder <guillermobukowski@gmail.com>
2025-03-06 10:27:57 -0300

    config de galeria de img de experiencias

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
 const carousel = document.querySelector('.carousel');
-const images = document.querySelectorAll('.carousel img');
+const imagesCarrousel = document.querySelectorAll('.carousel img');
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
+const images = document.querySelectorAll(".experiencias-item");
+const overlay = document.querySelector('.overlay');
 
 let index = 0;
-const totalImages = images.length;
+const totalImages = imagesCarrousel.length;
 
 function showSlide(newIndex) {
     if (newIndex < 0) {
@@ -23,3 +25,23 @@ nextBtn.addEventListener('click', () => showSlide(index + 1));
 
 // Cambio automático cada 5 segundos
 setInterval(() => showSlide(index + 1), 5000);
+
+
+
+
+images.forEach(image => {
+    image.addEventListener('click', (event) => {
+        event.stopPropagation();
+        console.log("click");
+        images.forEach (image => image.classList.remove('zoomed')); 
+        image.classList.toggle('zoomed')
+        overlay.style.display = 'block';
+        
+    })
+
+    document.addEventListener('click', () => {
+        image.classList.remove('zoomed');
+        overlay.style.display = 'none';
+    });
+});
+
