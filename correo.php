1b6d326fb0bf515ac3d1ff01916a33d9ecd68ccb
Traveller Coder <guillermobukowski@gmail.com>
2025-03-23 10:15:03 -0300

    config de archivo php para form

--- correo.php
+++ correo.php
@@ -1,53 +1,41 @@
 <?php
-$secret = '6LftLvcqAAAAAIw7rF9h-xqT8teBJqcbSQ5WqEIG';
+// reCAPTCHA
+$secret = '6Ld1tPcqAAAAAN__73qVky8cJInst8z1ZYLp-5wG';
 $response = $_POST['g-recaptcha-response'];
 $remoteip = $_SERVER['REMOTE_ADDR'];
 
 $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}&remoteip={$remoteip}");
 $captcha_success = json_decode($verify);
 
-    if 
-        ($captcha_success->success == false){;
-        echo "Eres un robot";
-    } else if 
-        ($captcha_success->success == true){;
-        $nombre = $_POST['name'];
+if (!$captcha_success->success) {
+    echo 'error_captcha';
+    exit;
+}
+
+// Datos del formulario
+$nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 
-$formcontent="
+// Contenido del correo
+$formcontent = "
     Datos de Contacto: \n
     Nombre: $nombre \n
     Email: $email \n
     Mensaje: $mensaje \n
 ";
 
+// Destinatario y asunto
 $recipient = "guillermobukowski@gmail.com";
-
 $subject = "Consulta de formulario de $nombre";
 
-$header = "From: $email \r\n";
+// Encabezado del correo
+$header = "From: $email\r\n";
 $header .= "Content-Type: text/plain; charset=UTF-8";
-mail($recipient, $subject, $formcontent, $header) or die("Error!");
-header("Location: /index");
-    }
-$nombre = $_POST['name'];
-$email = $_POST['email'];
-$mensaje = $_POST['mensaje'];
 
-$formcontent="
-    Datos de Contacto: \n
-    Nombre: $nombre \n
-    Email: $email \n
-    Mensaje: $mensaje \n
-";
-
-$recipient = "guillermobukowski@gmail.com";
-
-$subject = "Consulta de formulario de $nombre";
-
-$header = "From: $email \r\n";
-$header .= "Content-Type: text/plain; charset=UTF-8";
-mail($recipient, $subject, $formcontent, $header) or die("Error!");
-header("Location: /index");
-?>
+// Enviar correo
+if (mail($recipient, $subject, $formcontent, $header)) {
+    echo 'success';
+} else {
+    echo 'error';
+}
